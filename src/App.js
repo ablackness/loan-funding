15b0f86c794f5305ff4dafb4556b7475dc3feb51
Alex Black <alexblack426@gmail.com>
2019-03-30 09:17:27 -0700

    changes for deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import Modal from 'react-modal';
 import { 
   HashRouter as Router, 
   Route
@@ -7,55 +6,8 @@ import {
 import './App.css';
 import Display from './components/Display';
 import LoanInput from './components/LoanInput';
-// import Dialog from './components/Dialog';
-
-// Modal.setAppElement('#root');
-
-// const customStyles = {
-//   content : {
-//     top: '50%',
-//     left: '50%',
-//     right: 'auto',
-//     bottom: 'auto',
-//     marginRight: '-50%',
-//     transform: 'translate(-50%, -50%)',
-//     height: '175px',
-//     width: '400px'
-//   }
-// };
 
 class App extends Component {
-  // constructor() {
-  //   super();
-
-  //   this.state = {
-  //     modalIsOpen: false
-  //   };
-
-  //   this.openModal = this.openModal.bind(this);
-  //   this.afterOpenModal = this.afterOpenModal.bind(this);
-  //   this.closeModal = this.closeModal.bind(this);
-  // }
-
-  // openModal() {
-  //   this.setState({modalIsOpen: true});
-  // }
-
-  // afterOpenModal() {
-  //   // references are now sync'd and can be accessed.
-  //   // this.subtitle.style.color = '#f00';
-  // }
-
-  // closeModal() {
-  //   this.setState({modalIsOpen: false});
-  // }
-
-  // createDialog(title, message, yesButton, noButton) {
-  //   return (
-  //     <Dialog title message yesButton noButton/>
-  //   )
-  // }
-
   render() {
     return (
       <Router>
